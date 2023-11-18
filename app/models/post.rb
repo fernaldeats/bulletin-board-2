fc6a2289837c1c566e04e6209887d0799e2c015a
fernaldeats <42142633+fernaldeats@users.noreply.github.com>
2023-11-18 22:18:46 +0000

    only show forms if signed in

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,9 +9,11 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  board_id   :integer
+#  user_id    :integer
 #
 class Post < ApplicationRecord
   validates(:title, presence: true)
   validates(:body, presence: true)
   validates(:expires_on, presence: true)
+  belongs_to :user
 end
